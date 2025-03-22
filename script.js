af30c42b540ba110acb89d44148d25aa163db645
Swarajdhage <126773074+Swarajdhage@users.noreply.github.com>
2025-03-22 08:59:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript to change the background color when the button is clicked
+document.getElementById("changeColorButton").addEventListener("click", function() {
+    document.body.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
